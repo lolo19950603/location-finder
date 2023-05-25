10adf4cdffb2a128bd93c605404040b0e4021e55
loren950603 <loren840603@gmail.com>
2023-05-25 15:08:13 -0400

    Add CurrentLocation component and enable acquisition of current location from the browser using console.log command

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CurrentLocation msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CurrentLocation from './components/CurrentLocation.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CurrentLocation
   }
 }
 </script>
---
+++ src/components/CurrentLocation.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="current-location">
+    <button @click="getLocation">Acquire current locaiton</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CurrentLocation',
+  props: {
+    msg: String
+  },
+  methods: {
+    getLocation() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(this.handleSuccess, this.handleError);
+      }
+    },
+    handleSuccess(position) {
+      // Code to handle successful geolocation retrieval
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+      // Use latitude and longitude values as needed
+      console.log(latitude, longitude)
+    },
+    handleError(error) {
+      // Code to handle geolocation error
+      // error.code contains the error code, and error.message contains the error message
+      console.log(error)
+    },
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
