af5c086241b3df433514f6928662bad9cb0c7397
loren950603 <loren840603@gmail.com>
2023-05-31 18:06:27 -0400

    config for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === "production" ? "/location-finder/" : "/",
   transpileDependencies: true
 })
